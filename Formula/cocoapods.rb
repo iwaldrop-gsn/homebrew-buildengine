17594547b2e37ccc40066f5e883be399a3680830
Ian Waldrop <iwaldrop@gsngames.com>
2021-09-07 16:09:42 -0700

    fixed formulae, casks to point to intended versions

--- Formula/cocoapods.rb
+++ Formula/cocoapods.rb
@@ -1,16 +1,16 @@
-class CocoapodsAT1110 < Formula
+class Cocoapods < Formula
   desc "Dependency manager for Cocoa projects"
   homepage "https://cocoapods.org/"
-  url "https://github.com/CocoaPods/CocoaPods/archive/1.11.0.tar.gz"
-  sha256 "4f494e7651cdf1a7afae6117fb1ed33c919471d7bc3b7575a68d5c316faf567c"
+  url "https://github.com/CocoaPods/CocoaPods/archive/1.10.1.tar.gz"
+  sha256 "7629705179e4bfd894bebe4ed62c28d1cc539103f6d1924f4c8127f46cbd13e1"
   license "MIT"
+  revision 1
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "76747ed81b475d1f1ff107bc3b5c4d40428eae3de1fed1013dd4aa88d28d9f60"
-    sha256                               big_sur:       "e28afda81704ecd7620c3690a664c01477294ee724fefc8e98d2842ffdb87e9e"
-    sha256                               catalina:      "4743bfd539f5cf8440669c6aaa64da2c45ff1343f8953229d5156e7f1f5eb96b"
-    sha256 cellar: :any,                 mojave:        "0c11497ef552668f9d9e602e8031413f4513ba752e14a45782acfe0e72c12cd5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0fd089829f55aa89c6cb27584dab5a22955761fa62609d68c2660f1923329872"
+    sha256 cellar: :any, arm64_big_sur: "bf7b573944dfb6dee095e847102eb5595cafc69e56bd3e0f19e2bbe77b0b3e49"
+    sha256               big_sur:       "49e9b04847f8a3a667b3fa53cc1f66b001e461108c90704abe2981e7a560b0c7"
+    sha256               catalina:      "93a20a23596270d968a7b76f40f6a092218c75f53546ac8f0c84002df8d10974"
+    sha256 cellar: :any, mojave:        "d573ae11f98d5313564180a02c158173df17222cf81e7be1b8a370373e3e48fb"
   end
 
   depends_on "pkg-config" => :build
